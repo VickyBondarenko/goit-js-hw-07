e3cce06e4eecb0c24e67d3e50b4f10da91c8d836
VickyBondarenko <quikyv@gmail.com>
2023-01-26 21:17:01 +0100

    Add task1 and task2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+// import * as basicLightbox from "basiclightbox";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+function renderList() {
+  const markup = galleryItems
+    .map(
+      ({ preview, original, description }) => `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+    )
+    .join("");
+
+  gallery.innerHTML = markup;
+}
+renderList();
+
+gallery.addEventListener("click", openModal);
+
+function openModal(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName === "IMG") {
+    const instance = basicLightbox.create(`
+    <img src='${event.target.dataset.source}'>
+`);
+
+    instance.show();
+
+    if (instance.visible()) {
+      window.addEventListener("keydown", closeByEsc);
+
+      function closeByEsc({ code }) {
+        if (code === "Escape") {
+          instance.close();
+          window.removeEventListener("keydown", closeByEsc);
+        }
+      }
+    }
+  } else {
+    return;
+  }
+}
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+function renderList() {
+  const markup = galleryItems
+    .map(
+      ({
+        preview,
+        original,
+        description,
+      }) => `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}"  title="${description}"/>
+</a>`
+    )
+    .join("");
+
+  gallery.innerHTML = markup;
+}
+renderList();
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionPosition: "bottom",
+});
 
 console.log(galleryItems);
